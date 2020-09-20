ee68ff5d7a40567923d2b524d3417ca7a77b526f
Daniel Lawson <daniellawson9999@gmail.com>
2020-09-19 22:27:59 -0400

    udpated test/training script w/ checkpoints, training w/ old models

--- test_ppo.py
+++ test_ppo.py
@@ -1,33 +1,37 @@
 import gym
 import gym_yumi 
 from gym_yumi import envs
-from stable_baselines.common.policies import MlpPolicy
-from stable_baselines import PPO2
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
 def main(model_path):
     env = envs.YumiEnv('left','peg_target_res',mode='passive', headless=False,maxval=0.1)
+
+    # in function imports to fix env problem, normally move to top of script
+    from stable_baselines.common.policies import MlpPolicy
+    from stable_baselines import PPO2
     model = PPO2.load(model_path)
 
     episode_rewards = [0.0]
     obs = env.reset()
     done = False
     while (not done):
-        action = model.predict(obs)
+        # model.prection returns a tuple w/ (action, None) for some reason
+        action = model.predict(obs)[0]
+        #import pdb; pdb.set_trace()
         obs, reward, done, info = env.step(action)
-        epsiode_rewards.append(reward)
+        episode_rewards.append(reward)
     fig, ax = plt.subplots()
     ax.plot(episode_rewards)
     plt.show()
-
+    env.close()
 
 
 if __name__ == "__main__":
     assert (len(sys.argv) == 2), "python test_ppo.py [model_path]"
     model_path = sys.argv[1]
     main(model_path)
-
+    
  
 
--- train_ppo.py
+++ train_ppo.py
@@ -1,3 +1,6 @@
+import sys
+import os
+import numpy as np
 import gym
 import gym_yumi 
 from gym_yumi import envs
@@ -6,55 +9,45 @@ from stable_baselines import PPO2
 from stable_baselines.common import make_vec_env
 from stable_baselines.common import set_global_seeds
 from stable_baselines.common.vec_env import DummyVecEnv, SubprocVecEnv
+from stable_baselines.common.callbacks import CheckpointCallback
 
 
-# for some reason this crashes if performed after the stable_baselines imports?
-#env = make_vec_env('yumi-pegtransfer-v0')
+def main(new_model_name = "ppo2_yumi", old_model_name = None):
+    num_cpu = 8
+    timesteps = 2e6
+    save_freq = 1e5
+    def make_env(rank, seed=0):
+        """
+        Utility function for multiprocessed env.
+        
+        :param env_id: (str) the environment ID
+        :param num_env: (int) the number of environment you wish to have in subprocesses
+        :param seed: (int) the inital seed for RNG
+        :param rank: (int) index of the subprocess
+        """
+        def _init():
+            env = envs.YumiEnv('left','peg_target_res',mode='passive', headless=True,maxval=0.1)
+            env.seed(seed + rank)
+            return env
+        set_global_seeds(seed)
+        return _init
 
-def main(model_path = "ppo2_yumi"):
-  num_cpu = 6
+    env = SubprocVecEnv([make_env(i) for i in range(num_cpu)])
 
-  # envs = [envs.YumiEnv('left','peg_target_res',mode='passive', headless=True,maxval=0.1) for i in range(num_cpu)]
+    if old_model_name:
+        model = PPO2.load(os.path.join('./models/', old_model_name))
+    else:
+        model = PPO2(MlpPolicy, env, verbose=1, tensorboard_log=os.path.join('./tensorboard/ppo2', new_model_name))
+    checkpoint_callback = CheckpointCallback(save_freq=save_freq, save_path=os.path.join("./models", new_model_name, "checkpoints"))
+    model.learn(total_timesteps=int(timesteps), callback = checkpoint_callback)
+    model.save(os.path.join('./models', new_model_name))
 
-  def make_env(rank, seed=0):
-      """
-      Utility function for multiprocessed env.
-      
-      :param env_id: (str) the environment ID
-      :param num_env: (int) the number of environment you wish to have in subprocesses
-      :param seed: (int) the inital seed for RNG
-      :param rank: (int) index of the subprocess
-      """
-      def _init():
-          env = envs.YumiEnv('left','peg_target_res',mode='passive', headless=True,maxval=0.1)
-          env.seed(seed + rank)
-          return env
-      set_global_seeds(seed)
-      return _init
-
-  env = SubprocVecEnv([make_env(i) for i in range(num_cpu)])
-
-  model = PPO2(MlpPolicy, env, verbose=1, tensorboard_log='./tensorboard/ppo2/')
-  model.learn(total_timesteps=int(2e5))
-  model.save(model_path)
-
-  '''
-  obs = env.reset()
-  for i in range(100):
-      action, _states = model.predict(obs)
-      obs, rewards, dones, info = env.step(action)
-      env.render()
-
-  env.close()
-  '''
-
-  '''
-  lst_o += oh.get_position()  # position
-  ValueError: operands could not be broadcast together with shapes (0,) (3,) 
-  '''
 if __name__ == "__main__":
-  if (len(sys.argv) == 2):
-    model_path = sys.argv[1]
-    main(model_path)
-  else:
-    main()
+    # both models expected to be in the models folder
+    assert (len(sys.argv) >= 2), "try: python train_ppo.py [new_model_name] [old_model_name](optional)"
+    new_model_name = sys.argv[1]
+    if (len(sys.argv == 3)):
+        old_model_name = sys.argv[2]
+    else:
+        old_model_name = None
+    main(new_model_name, old_model_name)
