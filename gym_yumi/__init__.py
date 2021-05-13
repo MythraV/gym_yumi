f03d9b25788760b865be722a3e28ffaa11b20f96
Daniel Lawson <danielblawson@att.net>
2021-05-12 20:58:54 -0500

    fixed yumi-pegtransfer-v0 error

--- gym_yumi/__init__.py
+++ gym_yumi/__init__.py
@@ -4,7 +4,7 @@ register(
     id='yumi-pegtransfer-v0',
     entry_point='gym_yumi.envs:YumiEnv',
     reward_threshold=10.0,
-    kwargs={'limb':'left','goal':'peg_target_res','headless':False,'maxval':0.1}
+    kwargs={'limb':'left','goals': ['approach'],'headless':False,'maxval':0.1}
 )
 
 register(
